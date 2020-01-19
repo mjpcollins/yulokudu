d63a878ef23fd0d46f95421b41e9d41d862866f2
Matt Collins <matthew@Dees-MBP.localdomain>
2020-01-19 11:26:11 +0000

    port force 5000

--- utils/api_test.py
+++ utils/api_test.py
@@ -2,12 +2,16 @@
 import requests
 
 
-url = "http://35.234.147.205:80"
+url = "http://35.234.147.205:5000"
+# url = "http://127.0.0.1:5000"
 
 data = {"start_url": "https://www.businessinsider.com/peter-thiel-facebook-trump-biography-2018-2?r=US&IR=T",
         "article": "article9",
         "jumpdist": 0.5}
 
+print("send")
+
 res = requests.get(url, data)
 
 print(res.json())
+
